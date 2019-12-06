1465c003c3a26e9295bf6746befac06a98c899b2
Iris Martins <irisrfreitas@gmail.com>
2019-12-06 19:00:32 +0100

    Project created

---
+++ aulaJPA/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.remove(p);
+		em.getTransaction().commit();
+		
+
+		System.out.println("OK!!");
+		em.close();
+		emf.close();
+	}
+
+}
